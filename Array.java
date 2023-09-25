91b1498a13f7fdd4f50872e4418510110e88f3a2
carl conrad <carlconrad98071234@gmail.com>
2023-09-25 20:56:29 +0800

    array

--- Array.java
+++ Array.java
@@ -6,19 +6,22 @@ interface Arrays{
         void ascendingSort(int[] arr); //sort an array in  ascending way
         void descendingSort(int[] arr);//sort an array in descending way
         void edit(int index);//edit an element in the array
+        void displayArray(int[] my_array);
 }
 
 class MyArray implements Arrays{
    int[] arr;
-   int size;
+   int size,count;
     MyArray(int size){
         this.size = size;
         arr = new int[size];
+        count =0;
     }
 
     @Override
     public void add(int data) {
-      
+       arr[count] =data;
+       count++;
     }
 
     @Override
@@ -51,6 +54,40 @@ class MyArray implements Arrays{
        
     }
 
+    //Prints the elements stored in the array.
+    @Override
+    public void displayArray(int[] my_array) {
+        // the number of element printed depends on how many times the add() have been called 
+        // to prevent printing empty indices 
+        for(int i=0; i<getCount();i++){
+            System.out.print(my_array[i]+" ");
+        }
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
 
 
    
@@ -63,6 +100,11 @@ class MyArray implements Arrays{
 public class Array {
     
     public static void main(String[] args) {
-        
+        MyArray myArray = new MyArray(10);
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(0);
+       
+        myArray.displayArray(myArray.arr);
     }
 }
