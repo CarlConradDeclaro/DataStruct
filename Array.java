231e88b09bc659a9c70dbb770c814a756a300f1b
carl conrad <carlconrad98071234@gmail.com>
2023-09-25 20:37:50 +0800

    arrays

--- Array.java
+++ Array.java
@@ -1,4 +1,68 @@
+interface Arrays{
+        void add(int data); //add element to the array
+        void view(int data);//view an element if present in the array
+        void remove(int data);//remove an element from the array
+        void search(int data);//search for an element in the array
+        void ascendingSort(int[] arr); //sort an array in  ascending way
+        void descendingSort(int[] arr);//sort an array in descending way
+        void edit(int index);//edit an element in the array
+}
+
+class MyArray implements Arrays{
+   int[] arr;
+   int size;
+    MyArray(int size){
+        this.size = size;
+        arr = new int[size];
+    }
+
+    @Override
+    public void add(int data) {
+      
+    }
+
+    @Override
+    public void view(int data) {
+         
+    }
+
+    @Override
+    public void remove(int data) {
+      
+    }
+
+    @Override
+    public void search(int data) {
+      
+    }
+
+    @Override
+    public void ascendingSort(int[] arr) {
+         
+    }
+
+    @Override
+    public void descendingSort(int[] arr) {
+      
+    }
+
+    @Override
+    public void edit(int index) {
+       
+    }
+
+
+
+   
+
+
+
+}
+
+
 public class Array {
     
-    
+    public static void main(String[] args) {
+        
+    }
 }
