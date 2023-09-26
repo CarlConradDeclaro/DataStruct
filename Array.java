b8fd560f27a923c67f0142a265fa6c974ecd505d
carl conrad <carlconrad98071234@gmail.com>
2023-09-26 22:21:00 +0800

    array

--- Array.java
+++ Array.java
@@ -1,4 +1,4 @@
- 
+ import java.util.Scanner;
 
 interface Arrays{
         void add(int data); //add element to the array
@@ -7,8 +7,7 @@ interface Arrays{
         void search(int data);//search for an element in the array
         void ascendingSort(); //sort an array in  ascending way
         void descendingSort();//sort an array in descending way
-        void edit(int index,int data);//edit an element in the array
-        
+        void edit(int index,int data);//edit an element in the array       
 }
 
 class MyArray implements Arrays{
@@ -21,53 +20,68 @@ class MyArray implements Arrays{
     }
 
     @Override
-    public void add(int data) {
+    public void add(int data) {     
        arr[count] =data;
        count++;
-       System.out.println("New element added");
+       System.out.println("\n+++ New element added +++");
     }
 
     @Override
         public void viewArray(  ) {
-            for(int i=0; i<getCount();i++){
-                System.out.print(arr[i]+" ");
+     
+            String bars = ""; 
+            for(int k=0; k<getCount();k++){
+                bars+="+-+-";
             } 
+            System.out.println("\n\n"+bars);
+            for(int i=0; i<getCount();i++){
+                System.out.print(" "+arr[i]+" ");
+            }
             System.out.println();
+            System.out.println(bars);
+            
         }
 
     @Override
-    public  void remove(int index) {
-       
+    public void remove(int index) {
+         
+          if(index > getSize()-1){
+                 System.out.println("<<<"+index + " is OutOfBounds>>>");
+                 return;
+             }
+             
          setCount(getCount()-1);// decrease the Count of the array because we're going to remove 1 element
          int n =getCount();//
          int last = arr[getCount()];  // get the lst element 
          int j=0;
+        
           for(int i=0;i<n;i++){
               if(i != index){
                 arr[j] =arr[i];              
-                j++;
-              }   
+                j++;             
+              }               
           }
           // put the last element here after iterationg the updated elements of the array
-           arr[j] = last;
-           System.out.println(arr[index] + " has been removed");
+           arr[j] = last;       
+           System.out.println("\n+++"+arr[index--] + " has been removed+++");
     }
 
     @Override
     public void search(int data) {
+  
        int low =0;
        int high = arr.length-1;
-         
+                      
             while(low<=high){
                 int mid1 = low+ (high-low)/3;
                 int mid2 = high- (high-low)/3;
 
                     if(data == arr[mid1]){
-                        System.out.println("Element: " + data + " is present at index: " + mid1);
+                        System.out.println("\n+++ Element: " + data + " is present at index: " + mid1+ "+++");
                         return;
                     }
                     if(data == arr[mid2]){
-                        System.out.println("Element: " + data + " is present at index: " + mid2);
+                        System.out.println("\n+++ Element: " + data + " is present at index: " + mid2+ "+++");
                         return;
                     }
                     if(data < arr[mid1]){
@@ -79,12 +93,12 @@ class MyArray implements Arrays{
                         low = mid1+1;
                     }
                } 
-       System.out.println(data + " not found");   
+       System.out.println("\n<<<"+data + " not found>>>");   
     }
 
     @Override
     public void ascendingSort() {
-       
+                
 	     for(int i=0;i<getCount();i++){
 	         int min=i; // 7
 	         for(int j=i+1;j<getCount();j++){
@@ -95,12 +109,12 @@ class MyArray implements Arrays{
 	         arr[min] = arr[i];  
 	         arr[i] = x; 
 	     }   
-        System.out.println("The array has been sorted ascendingly");           
+        System.out.println("\n+++ The array has been sorted ascendingly +++");           
     }
 
     @Override
     public void descendingSort( ) {
-          
+               
         for(int i=0;i<getCount();i++){
 	         int min=i; // 7
 	         for(int j=i+1;j<getCount();j++){
@@ -111,17 +125,28 @@ class MyArray implements Arrays{
 	         arr[min] = arr[i];  
 	         arr[i] = x; 
 	     }
-         System.out.println("The array has been sorted descendingly.");           
+         System.out.println("\n+++ The array has been sorted descendingly. +++");           
     }
 
     @Override
     public void edit(int index, int data) {
-             System.out.println("Element "+ arr[index] + " has been change to " +data);
+             if(index > getSize()-1){
+                 System.out.println("<<<"+index + " is OutOfBounds>>>");
+                 return;
+             }
+             System.out.println("\nElement "+ arr[index] + " has been change to " +data);
              arr[index] = data; 
     }  
 
     public int[] getArr() {
         return arr;
+    }
+    
+    boolean isEmpty(){
+        return getCount() == 0 ? true : false;
+    }
+    boolean isFull(){
+        return getCount() > getSize()-1 ? true : false;
     }
   
     public void setArr(int[] arr) {
@@ -146,11 +171,81 @@ public class Array {
     
     public static void main(String[] args) {
 
-         System.out.println("Welcome enter desired size for your array");
-
-        MyArray myArray = new MyArray(10);
-     
-                  
+        Scanner sc = new Scanner(System.in);
+                
+        System.out.print("\n            Welcome. \nEnter desired size for your array \nSize: ");
+        int size = sc.nextInt();
+        MyArray myArray = new MyArray(size);
+        myArray.setSize(size);
+        System.out.println("\nMyArray myArray = new MyArray(" +myArray.getSize()+");");
+        
+        boolean run = true;
+        do{
+            System.out.print("\nOperations: \n 1,Add\n 2,Ascending Sort\n 3,Descending Sort\n 4,Edit\n 5,Remove\n 6,ViewArray\n 7,Exit \nEnter: ");
+            int operation  = sc.nextInt();
+            switch(operation){
+                case 1 :
+                    if(myArray.isFull()){
+                        System.out.println("\n<<<Array is Full!>>>");
+                        break;
+                    }else{
+                      System.out.print("Enter Element: ");
+                      myArray.add(sc.nextInt());
+                    }
+                    break;
+                case 2: 
+                    if(myArray.isEmpty()){
+                        System.out.println("\n<<<Array is is Empty!>>>");
+                        break;
+                    }else{
+                    myArray.ascendingSort();
+                    }
+                    break;
+                case 3:
+                     if(myArray.isEmpty()){
+                        System.out.println("\n<<<Array is is Empty!>>>");
+                        break;
+                    }else{
+                    myArray.descendingSort();
+                    }
+                    break;
+                case 4: 
+                     if(myArray.isEmpty()){
+                        System.out.println("\n<<<Array is is Empty!>>>");
+                        break;
+                    }else{
+                        myArray.viewArray();
+                        System.out.print("Enter index: ");
+                        int index = sc.nextInt();
+                        System.out.print("Enter new element: ");
+                        int data = sc.nextInt();
+                        myArray.edit(index,data);
+                    }
+                    break;
+                case 5:
+                      if(myArray.isEmpty()){
+                        System.out.println("\n<<<Array is is Empty!>>>");
+                        break;
+                    }else{
+                    myArray.viewArray();
+                    System.out.print("Enter index to remove: ");
+                    myArray.remove(sc.nextInt());
+                    }
+                    break;
+                case 6: 
+                     if(myArray.isEmpty()){
+                        System.out.println("\n<<<Array is is Empty!>>>");
+                        break;
+                    }else{
+                    myArray.viewArray();
+                    }
+                    break;
+               
+                default:
+                    run =false;
+                    break;
+            }                           
+        }while(run);                
     }
 }
 
